94f4098443739768b2b5f16c238cd21812c18c69
ygboucherk <ygboucherk@gmail.com>
2023-02-26 23:19:17 +0100

    Add ad in navbar

--- scripts/loadend.js
+++ scripts/loadend.js
@@ -7,6 +7,8 @@ navContent = `
     <a href="dualsun.html"><button class="navButton">Hybride dualsun</button></a>
     <a href="rentabilite.html"><button class="navButton">Simulateur</button></a>
     <div class="adContainer">
+		<iframe data-aa="2176899" src="//acceptable.a-ads.com/2176899?size=Adaptive&background_color=230a0a&text_color=f2e9e9" style="border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;"></iframe>
+	
         <!-- Pour mettre des pubs lol --!>
     </div>
 `
